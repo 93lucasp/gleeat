fbde3d9a46d87858f734959e27e95597d99435e0
93lucasp <spezzanoluca@gmail.com>
2016-04-07 20:44:15 -0700

    merging

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/gleeat");
+mongoose.connect(process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL || "mongodb://localhost/gleeat");
 module.exports.User = require("./user");
 module.exports.Post = require("./post");
